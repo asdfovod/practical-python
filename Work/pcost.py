3f0d6a0558ca208566ace9252cef5885582aeef9
asdfovod <ylesiw1209@gmail.com>
2022-11-04 22:08:32 +0900

    update

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
-sum = 0
-import gzip
-with gzip.open('Work/Data/portfolio.csv.gz', 'rt') as f:
-    headers = next(f)
-    dataList = []
-    for line in f:
-        name, shares, price = line.strip().split(',')
-        sum += float(shares) * float(price)
-print('Total cost', sum)
+import sys
+
+def portfolio_cost(filename):
+    sum = 0
+    with open(filename, 'rt') as f:
+        next(f) # header
+        for line in f:
+            name, shares, price = line.strip().split(',')
+            sum += float(shares) * float(price)
+    return sum
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
+
