05f9bd4575d6312d62f3c00f6078b186cbc47ed9
asdfovod <ylesiw1209@gmail.com>
2022-11-09 01:51:41 +0900

    dd

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,28 @@
 # pcost.py
 #
 # Exercise 1.27
-import sys
+import sys, csv
 
 def portfolio_cost(filename):
-    sum = 0
+    total_cost = 0
     with open(filename, 'rt') as f:
-        next(f) # header
-        for line in f:
-            name, shares, price = line.strip().split(',')
-            sum += float(shares) * float(price)
-    return sum
+        rows = csv.reader(f)
+        headers = next(rows) # header
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
+            try:
+                # name, shares, price = rows.strip().split(',')
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += nshares * price
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
+    return total_cost
 
-if len(sys.argv) == 2:
+if len(sys.argv) == 2 or not FileNotFoundError:
     filename = sys.argv[1]
 else:
+    print('File not found')
     filename = 'Data/portfolio.csv'
 cost = portfolio_cost(filename)
 print('Total cost:', cost)
