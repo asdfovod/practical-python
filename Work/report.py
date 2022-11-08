7f1b897b921c41b3766924b467d858e7ee99efb7
asdfovod <ylesiw1209@gmail.com>
2022-11-09 01:50:16 +0900

    delete comment

--- Work/report.py
+++ Work/report.py
@@ -13,10 +13,6 @@ def read_portfolio(filename):
         headers = next(rows)
         for nrow, row in enumerate(rows, start=1):
             holding = dict(zip(headers, row))
-            # holding = dict()
-            # holding[headers[0]] = row[0]
-            # holding[headers[1]] = int(row[1])
-            # holding[headers[2]] = float(row[2])
             portfolio.append(holding.copy()) # 가변자료형은 얕은복사x
     return portfolio
 
@@ -28,9 +24,6 @@ def read_price(filename):
         for nrow, row in enumerate(rows, start=1):
             try:
                 holding = dict(zip(headers, row))
-                # holding = dict()
-                # holding['name'] = row[0]
-                # holding['price'] = float(row[1])
                 price.append(holding.copy()) # 가변자료형은 얕은복사x
             except IndexError:
                 break        
@@ -75,34 +68,3 @@ for pf in portfolio:
             prices = '$' + str(pf['price']) # 여기부터안됨
             
             print(f'{name:>10s} {shares:>10s} {prices:>10s} {change:>10.2f}')
-            # print('{:>10s} {:>10s} {:>10.2s} {:>10.2f}'.format(pf['name'], pf['shares'], pr['price'], float(pr['price']) - float(pf['price'])))
-
-# import sys
-# import csv
-
-# def read_portfolio(filename):
-#     portfolio = {}
-#     with open(filename, 'rt') as f:
-#         rows = csv.reader(f)
-#         headers = next(rows)
-#         for row in rows:
-#             holding = (row[0], int(row[1]), float(row[2]))
-#             portfolio.append(holding)
-#     return portfolio
-
-# def portfolio_cost(portfolio):
-#     total_cost = 0
-#     for i in portfolio:
-#         cost = i[1] * i[2]
-#         total_cost += cost
-#     return total_cost
-
-# if len(sys.argv) == 2:
-#     filename = sys.argv[1]
-# else:
-#     filename = 'Data/portfolio.csv'
-
-# portfolio = read_portfolio(filename)
-# cost = portfolio_cost(portfolio)
-# print('Total cost:', cost)
-
